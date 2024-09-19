4d6153834d05042188319780c5db2d434f0b4347
mingxue619 <mingxue619@gmail.com>
2024-09-19 16:09:51 +0800

    tgypst

--- server/plugin/typst.js
+++ server/plugin/typst.js
@@ -1,16 +1,29 @@
 import { NodeCompiler } from "@myriaddreamin/typst-ts-node-compiler";
 
-class Compiler {
+class Typst {
     static compiler;
     // https://myriad-dreamin.github.io/typst.ts/cookery/guide/all-in-one-node.html
-    static svg(code) {
-        if (!Compiler.compiler) {
-            Compiler.compiler = NodeCompiler.create();
+    static instance() {
+        if (!Typst.compiler) {
+            Typst.compiler = NodeCompiler.create();
         } else {
-            Compiler.compiler.evictCache(10);
+            Typst.compiler.evictCache(10);
         }
+    }
 
-        return Compiler.compiler.svg({ mainFileContent: code });
+    static compile(code) {
+        Typst.instance();
+        return Typst.compiler.compile({ mainFileContent: code });
+    }
+
+    static svg(code) {
+        Typst.instance();
+        return Typst.compiler.svg({ mainFileContent: code });
+    }
+
+    static plainSvg(code) {
+        Typst.instance();
+        return Typst.compiler.plainSvg({ mainFileContent: code });
     }
 }
 function typstWrapper(code) {
@@ -40,11 +53,26 @@ function markdownitTypst(md) {
     });
 
     md.renderer.rules.typst = (tokens, idx, opts, env, self) => {
-        debugger;
         const code = typstWrapper(tokens[idx].content);
-        const svg = Compiler.svg(code);
-        const result = svgWrapper(svg) + "\n";
-        return result;
+        try {
+            const svg = Typst.svg(code);
+            const result = svgWrapper(svg) + "\n";
+            return result;
+        } catch (error) {
+            const errorMessage = JSON.stringify(error);
+            const compile = Typst.compile(code);
+            const diagnostics = compile.takeDiagnostics();
+            if (diagnostics && diagnostics.compilationStatus === "error") {
+                const shortDiagnostics = diagnostics.shortDiagnostics;
+                const len = shortDiagnostics.length;
+                const diagnoseMessage = JSON.stringify(shortDiagnostics);
+                const diagnoseResult = `<pre><code style="color: red;">error::<br>${errorMessage} <br><br>diagnose:: ${len} compiler error<br>${diagnoseMessage}</code></pre>`;
+                return diagnoseResult;
+            } else {
+                const errorResult = `<pre><code style="color: red;">error::<br>${errorMessage}</code></pre>`;
+                return errorResult;
+            }
+        }
     };
 }
 export { markdownitTypst };
