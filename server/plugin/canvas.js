a698903a04a4ab3133ea10761b701a86733f71fd
mingxue619 <mingxue619@gmail.com>
2024-09-20 14:51:49 +0800

    canvas

--- server/plugin/canvas.js
+++ server/plugin/canvas.js
@@ -1,60 +1,95 @@
+function getAttributes(info) {
+    const attributes = {};
+    const canvasRegex = /canvas\(([^)]+)\)/;
+    const match = info.match(canvasRegex);
+    if (match && match[1]) {
+        const attributesString = match[1];
+        const attributePairs = attributesString.split(",");
+        attributePairs.forEach((pair) => {
+            const [key, value] = pair.trim().split("=");
+            attributes[key.trim()] = value.replace(/^['"]|['"]$/g, "");
+        });
+    }
+    return attributes;
+}
+function getId(attributes) {
+    let id = attributes.id;
+    if (!id) {
+        id =
+            "canvas-uuid-" +
+            "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+                var r = (Math.random() * 16) | 0,
+                    v = c == "x" ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+            });
+        attributes["id"] = id;
+    }
+    return id;
+}
 function markdownitCanvas(md) {
+    md.core.ruler.after("block", "canvas", (state) => {
+        for (let i = 0; i < state.tokens.length; i++) {
+            const token = state.tokens[i];
+
+            if (token.type !== "fence") {
+                continue;
+            }
+
+            const isCanvas = token.info.startsWith("canvas");
+
+            if (!isCanvas) {
+                continue;
+            }
+
+            token.type = "canvas";
+        }
+    });
+
     const proxy = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
     const defaultFenceRenderer = md.renderer.rules.fence || proxy;
 
-    md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+    md.renderer.rules.canvas = function (tokens, idx, options, env, self) {
         let token = tokens[idx];
-        if (token && token.info) {
-            let info = token.info;
-            if (info.startsWith("canvas")) {
-                const attributes = {};
-                const canvasRegex = /canvas\(([^)]+)\)/;
-                const match = info.match(canvasRegex);
-                if (match && match[1]) {
-                    const attributesString = match[1];
-                    const attributePairs = attributesString.split(",");
-                    attributePairs.forEach((pair) => {
-                        const [key, value] = pair.trim().split("=");
-                        attributes[key.trim()] = value.replace(/^['"]|['"]$/g, "");
-                    });
-                }
-                let id = attributes.id;
-                if (!id) {
-                    id =
-                        "canvas-uuid-" +
-                        "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-                            var r = (Math.random() * 16) | 0,
-                                v = c == "x" ? r : (r & 0x3) | 0x8;
-                            return v.toString(16);
-                        });
-                    attributes["id"] = id;
-                }
-                let context = attributes.context || "2d";
-                let content = token.content || "";
-                let canvasAttribute = Object.entries(attributes)
-                    .map(([k, v]) => `${k}="${v}"`)
-                    .join(" ");
-                const tag = `
-                            <canvas class="canvas" ${canvasAttribute}>
-                                browser not support canvas
-                            </canvas>`;
-                const script = `
-                               <script>
-                                   (function() {
-                                       // debugger;
-                                       let element = document.getElementById("${id}");
-                                       if (element.getContext) {
-                                           let canvas = element.getContext("${context}");
-                                           canvas.clearRect(0, 0, element.width, element.height);
-                                           ${content}
-                                       };
-                                   })();
-                               </script>`;
-                const html = tag + script;
-                return html;
-            }
-        }
-        return defaultFenceRenderer(tokens, idx, options, env, self);
+        let info = token.info;
+        let attributes = getAttributes(info);
+        let id = getId(attributes);
+        let errorId = "error-" + id;
+        let context = attributes.context || "2d";
+        let content = token.content || "";
+        let canvasAttribute = Object.entries(attributes)
+            .map(([k, v]) => `${k}="${v}"`)
+            .join(" ");
+        const tag = `
+                    <canvas class="canvas" ${canvasAttribute}>
+                        browser not support canvas
+                    </canvas>
+                    <div id="${errorId}" style="display: none"></div>
+                    `;
+        const script = `
+                        <script>
+                            (function() {
+                                // debugger;
+                                let errorElement = document.getElementById('${errorId}');  
+                                try {
+                                    let element = document.getElementById("${id}");
+                                    if (element.getContext) {
+                                        let canvas = element.getContext("${context}");
+                                        canvas.clearRect(0, 0, element.width, element.height);
+                                        ${content}
+                                    };
+                                } catch (error) {
+                                    errorElement.style.display = "block";
+                                    let code = document.createElement('code');  
+                                    code.style.color = "red";
+                                    code.textContent = error.stack;  
+                                    let pre = document.createElement('pre');  
+                                    pre.appendChild(code);  
+                                    document.getElementById('${errorId}').appendChild(pre);
+                                }
+                            })();
+                        </script>`;
+        const html = tag + script;
+        return html;
     };
-};
+}
 export { markdownitCanvas };
