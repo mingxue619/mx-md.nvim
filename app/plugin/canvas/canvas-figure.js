d6a3f73f1e7ba43ab5f8c96f37578f9f5ba4deca
mingxue619 <mingxue619@gmail.com>
2024-09-26 11:26:21 +0800

    figure

--- app/plugin/canvas/canvas-figure.js
+++ app/plugin/canvas/canvas-figure.js
@@ -40,11 +40,11 @@ function Figure(ctx) {
                 x = position.x - textWidth / 2;
                 y = position.y + textHeight / 2;
             } else {
-                let { frame = { left: 0, right: 100, top: 0, bottom: 100, padding: 1 }, v = "center", h = "center" } = align || {};
+                let { frame = { left: 0, right: 100, top: 0, bottom: 100 }, margin = 1, v = "center", h = "center" } = align || {};
                 if (h === "left") {
-                    x = frame.left + frame.padding;
+                    x = frame.left + margin;
                 } else if (h === "right") {
-                    x = frame.right - textWidth - frame.padding;
+                    x = frame.right - textWidth - margin;
                 } else if (h === "center") {
                     const fw = frame.right - frame.left;
                     let spaceW = fw - textWidth;
@@ -54,9 +54,9 @@ function Figure(ctx) {
                     throw new Error("label.align.h is error");
                 }
                 if (v === "top") {
-                    y = frame.top + textHeight;
+                    y = frame.top + textHeight + margin;
                 } else if (v === "bottom") {
-                    y = frame.bottom + frame.padding;
+                    y = frame.bottom + margin;
                 } else {
                     const fh = frame.bottom - frame.top;
                     let spaceH = fh - textHeight;
