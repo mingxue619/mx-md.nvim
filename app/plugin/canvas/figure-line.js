a1d63d94c3e9bc7ab23d82a463de4e8e7a2f6bbc
mingxue619 <mingxue619@gmail.com>
2024-09-27 15:59:22 +0800

    figure

--- app/plugin/canvas/figure-line.js
+++ app/plugin/canvas/figure-line.js
@@ -2,7 +2,19 @@ export class Line {
     constructor(ctx) {
         this.ctx = ctx;
     }
-    draw({ from, to }) {
-        debugger
+    draw({ from, to, style }) {
+        debugger;
+        const ctx = this.ctx;
+        // style
+        const { strokeStyle, lineWidth } = style || {};
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(from[0], from[1]);
+        ctx.lineTo(to[0], to[1]); // 从左上角到右上角
+        ctx.strokeStyle = strokeStyle;
+        ctx.lineWidth = lineWidth;
+        ctx.stroke();
+        ctx.restore();
     }
 }
