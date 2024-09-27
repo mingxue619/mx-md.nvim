4363354c55f065123f0978cec6ecc5d56fe886da
mingxue619 <mingxue619@gmail.com>
2024-09-27 16:13:00 +0800

    figure line

--- app/plugin/canvas/figure-line.js
+++ app/plugin/canvas/figure-line.js
@@ -6,15 +6,42 @@ export class Line {
         debugger;
         const ctx = this.ctx;
         // style
-        const { strokeStyle, lineWidth } = style || {};
+        const { strokeStyle, lineWidth, fillStyle } = style || {};
 
+        const startX = from[0];
+        const startY = from[1];
+        const endX = to[0];
+        const endY = to[1];
+        // draw line
         ctx.save();
         ctx.beginPath();
-        ctx.moveTo(from[0], from[1]);
-        ctx.lineTo(to[0], to[1]); // 从左上角到右上角
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
         ctx.strokeStyle = strokeStyle;
         ctx.lineWidth = lineWidth;
         ctx.stroke();
+
+        // 计算箭头的位置
+        const dx = endX - startX;
+        const dy = endY - startY;
+        const angle = Math.atan2(dy, dx); // 角度
+        const arrowLength = 10; // 箭头长度
+        // 计算箭头的两个顶点
+        const arrowX1 = endX - arrowLength * Math.cos(angle - Math.PI / 6);
+        const arrowY1 = endY - arrowLength * Math.sin(angle - Math.PI / 6);
+        const arrowX2 = endX - arrowLength * Math.cos(angle + Math.PI / 6);
+        const arrowY2 = endY - arrowLength * Math.sin(angle + Math.PI / 6);
+        // 绘制箭头
+        ctx.beginPath();
+        ctx.moveTo(endX, endY);
+        ctx.lineTo(arrowX1, arrowY1);
+        ctx.lineTo(arrowX2, arrowY2);
+        ctx.closePath(); // 闭合路径以绘制箭头
+        ctx.stroke();
+        ctx.fillStyle = fillStyle || strokeStyle || window.foreground;
+        ctx.fill();
+
+
         ctx.restore();
     }
 }
