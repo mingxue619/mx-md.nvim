df546b77a80a70b3566b1a832aacbac2e3734344
mingxue619 <mingxue619@gmail.com>
2024-09-29 16:51:51 +0800

    figure triangle

--- app/plugin/canvas/figure-triangle.js
+++ app/plugin/canvas/figure-triangle.js
@@ -15,10 +15,9 @@ export class Triangle {
         const { strokeStyle, lineWidth, fill, fillStyle } = style || {};
         // draw
         ctx.beginPath();
-        ctx.moveTo(from.x, from.y);
-        ctx.lineTo(from.x + width, from.y); // 从左上角到右上角
-        ctx.lineTo(from.x + width, from.y + height); // 从右上角到右下角
-        ctx.lineTo(from.x, from.y + height); // 从右下角到左下角
+        ctx.moveTo(from.x + width / 2, from.y); // 上顶点
+        ctx.lineTo(from.x + width, from.y + height); // 右下角
+        ctx.lineTo(from.x, from.y + height); // 左下角
         ctx.closePath(); // 闭合路径，即从左下角回到左上角
         // 绘制边框
         ctx.strokeStyle = strokeStyle;
