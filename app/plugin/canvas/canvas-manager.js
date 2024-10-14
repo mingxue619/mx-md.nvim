da23cb4264076e0fcec7e498b043a8f998da1e2b
mingxue619 <mingxue619@gmail.com>
2024-10-14 10:34:34 +0800

    canvas

--- app/plugin/canvas/canvas-manager.js
+++ app/plugin/canvas/canvas-manager.js
@@ -2,7 +2,7 @@ import { Axes } from "/app/plugin/canvas/canvas-axes.js";
 import { CanvasScroll } from "/app/plugin/canvas/canvas-scroll.js";
 
 export class CanvasManager {
-    static recursionDrawFlag = false;
+    static recursionDrawShape = false;
     static paintings = [];
     static dispatchPaintingFinishEvent(painting) {
         const paintingFinishEvent = new CustomEvent("PaintingFinishEvent", {
@@ -53,7 +53,7 @@ export class CanvasManager {
         if (focusShape) {
             CanvasManager.drawShape(focusShape);
         }
-        if (CanvasManager.recursionDrawFlag) {
+        if (CanvasManager.recursionDrawShape === true) {
             requestAnimationFrame(() => {
                 CanvasManager.recursionDraw(painting);
             });
