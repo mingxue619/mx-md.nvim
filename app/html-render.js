a114d98bf0bdd49775e3ca9902a429791b22642d
mingxue619 <mingxue619@gmail.com>
2024-11-29 15:42:36 +0800

    canvas

--- app/html-render.js
+++ app/html-render.js
@@ -8,11 +8,18 @@ export class HtmlRender {
         if (this.hash === newHash) {
             return;
         }
+        //title
+        const filePath = bufferInfo.name;
+        const fileName = filePath.split('/').at(-1).split('.')[0];
+        const pageTitle = bufferInfo.pageTitle;
+        debugger
+        // html
         const html = bufferInfo.html;
         const newHtml = `<section>${html}</section>`;
         // console.log(Object.keys(md.renderer.rules));
         const contentElement = document.getElementById("content");
         contentElement.innerHTML = newHtml;
+        // script
         let scripts = contentElement.getElementsByTagName("script");
         Array.from(scripts).forEach(function (script) {
             let scriptElement = document.createElement("script");
