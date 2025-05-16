324da47b825be7b7fea170ec927359d75ac7c68c
mingxue619 <mingxue619@gmail.com>
2025-05-16 17:40:11 +0800

    graphviz

--- app/plugin/graphviz/graphviz-render.js
+++ app/plugin/graphviz/graphviz-render.js
@@ -1,5 +1,14 @@
+import { instance } from "/node_modules/@viz-js/viz/lib/viz-standalone.mjs";
+
 export class GraphvizRender {
     static async init(bufferInfo) {
-        debugger
+        const viz = await instance();
+        const elements = document.getElementsByClassName("dot");
+        const elementArray = Array.from(elements);
+        elementArray.forEach((element) => {
+            const svg = viz.renderSVGElement(element.textContent);
+            element.textContent = "";
+            element.appendChild(svg);
+        });
     }
 }
